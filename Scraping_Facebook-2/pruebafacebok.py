a6b5ef303f319230567e27a47040111c20bafe08
joel1844 <friascastillo0403@gmail.com>
2022-03-01 03:38:36 +0800

    scroll

--- Scraping_Facebook-2/pruebafacebok.py
+++ Scraping_Facebook-2/pruebafacebok.py
@@ -6,9 +6,9 @@ import time
 from selenium.webdriver.chrome.service import Service
 from dotenv import load_dotenv
 import os
-from proxy import get_proxy_arr
+#from proxy import get_proxy_arr
 
-proxy_list = get_proxy_arr()
+#proxy_list = get_proxy_arr()
 # load_dotenv()
 # PATH = os.getenv('W_PATH')
 PATH = "C:/Users/frias/OneDrive - Defensor del Pueblo/Desktop/chromedriver"
@@ -26,68 +26,107 @@ options.add_argument('--ignore-ssl-errors')
 
 #options.add_argument('--user-agent=%s' % ua)
 #caps['acceptInsecureCerts'] = True
-url = 'https://www.facebook.com/'
+url = 'https://facebook.com/'
 
-for eachProxy in proxy_list[1:]:
+# for eachProxy in proxy_list[1:]:
 
-    try:
+
+def scroll(driver):
+    SCROLL_PAUSE_TIME = 5
+
+    # Get scroll height
+    last_height = driver.execute_script("return document.body.scrollHeight")
+
+    while True:
+        window.find_elements(By.TAG_NAME, 'article')
+            
+        # Scroll down to bottom
+        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+
+        # Wait to load page
+        time.sleep(SCROLL_PAUSE_TIME)
+
+        # Calculate new scroll height and compare with last scroll height
+        new_height = driver.execute_script("return document.body.scrollHeight")
+        if new_height == last_height:
+            break
+        last_height = new_height
         
-        options.add_argument('--proxy-server=%s' % eachProxy['proxy'])
-        print('--proxy-server=%s' % eachProxy['proxy'])
-        driver = webdriver.Chrome(service = s,options=options)
-        driver.get(url)
-        time.sleep(10)
-    except:
-        print(eachProxy["proxy"] + " is not working")
- 
-    username =driver.find_element(By.ID, 'email') 
-    password = driver.find_element(By.ID, 'pass')    
-    username.send_keys('jc7645085@gmail.com') 
-    password.send_keys('Frias123vv') 
-    enter = driver.find_element(By.NAME,'login') 
-    time.sleep(5)
-    enter.click() 
+        print(len(window.find_elements(By.TAG_NAME, 'article')))
+    
+    return window.find_elements(By.TAG_NAME, 'article')
+
+try:
+    PROXY = "154.16.89.172:45785"
+    #options.add_argument('--proxy-server=%s' % PROXY)
+    driver = webdriver.Chrome(service = s,options=options)
+    driver.get(url)
+    time.sleep(10)
+except:
+    print("hello")
+
+
+username =driver.find_element(By.ID, 'email') 
+password = driver.find_element(By.ID, 'pass')    
+username.send_keys('jc7645085@gmail.com') 
+password.send_keys('Frias123vv') 
+enter = driver.find_element(By.NAME,'login') 
+time.sleep(5)
+enter.click() 
+time.sleep(10) 
+with driver as window:  
+    window.get("https://m.facebook.com/DefensorRD") 
     time.sleep(10) 
-    with driver as window:  
-        window.get("https://m.facebook.com/DefensorRD") 
-        time.sleep(10) 
-    
-        container = window.find_elements(By.TAG_NAME,"article") 
-        for element in container: 
-            titulo = None 
-            fuente = 'facebook' 
-            linkk = None 
-            cometarios = None 
-            megusta = None
-            coment = None
-            shared = None
 
-            titulo = element.find_element(By.CLASS_NAME,'_2pim').text 
-            megusta = element.find_element(By.CLASS_NAME,'_1g06').text
-            sharedcomenst = element.find_element(By.CLASS_NAME,'_1j-c').text
+    
+    container = scroll(driver)
+    print(len(container))
+    for element in container: 
+        titulo = None   
+        fuente = 'facebook' 
+        linkk = None 
+        cometarios = None 
+        likes = None
+        coment = None
+        shared = None
+
         
-            try: 
-                linkk = element.find_element(By.CLASS_NAME,'_5msj') 
-                if linkk: 
-                    linkk = linkk.get_attribute('href') 
-            except Exception as e: 
-                print(e) 
-                pass 
-            try: 
-                with window as window: 
-                    window.get(linkk) 
-                    cometarios = window.find_element(By.CLASS_NAME,"_2b06").text
-                    #shared = window.find_element(By.XPATH,"//div[@data-sigil='share-count']").text
+        titulo = element.find_element(By.CLASS_NAME,'_5rgt').text 
+        try:
+            likes = element.find_element(By.CLASS_NAME,'_1g06').text
+        except Exception as e:
+             likes = None
 
-                    time.sleep(10)
-
-                    atras = window.find_element(By.CLASS_NAME,"_6j_c").click() 
-
-                    time.sleep(10)
-
-            except Exception as e: 
-                print(e) 
-                time.sleep(16)
+        try:
+            sharedcomenst = element.find_element(By.CLASS_NAME,'_1j-c').text
+        except Exception as e:
+            sharedcomenst = None
     
-            dic = dict(titulo = titulo, fuente = fuente, link = linkk,likes = megusta,sharedorcoments = sharedcomenst,comentarios = cometarios) 
-            print(dic)
+        try: 
+            linkk = element.find_element(By.CLASS_NAME,'_5msj') 
+            if linkk: 
+                linkk = linkk.get_attribute('href') 
+        except Exception as e: 
+            print(e) 
+            pass 
+        try: 
+            with webdriver.Chrome(PATH) as window: 
+                window.get(linkk) 
+                try:
+                    cometarios = window.find_element(By.CLASS_NAME,'_2b06').text
+                except Exception as e:
+                    cometarios = None
+                #shared = window.find_element(By.XPATH,"//div[@data-sigil='share-count']").text
+
+                time.sleep(10)
+
+        except Exception as e: 
+            print(e) 
+            time.sleep(16)
+
+        dic = dict(titulo = titulo, fuente = fuente, link = linkk,likes = likes,sharedorcoments = sharedcomenst,comentarios = cometarios) 
+        print(dic)
+
+
+
+        
