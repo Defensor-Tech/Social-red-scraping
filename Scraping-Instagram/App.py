76c0861803d353cfd28ad995a0e05f6955d2f9a1
joel1844 <friascastillo0403@gmail.com>
2022-03-11 05:34:41 +0800

    finished

--- Scraping-Instagram/App.py
+++ Scraping-Instagram/App.py
@@ -20,8 +20,13 @@ driver.delete_all_cookies()
 username = WebDriverWait(driver,10).until(EC.element_to_be_clickable((By.CSS_SELECTOR,"input[name='username']"))) 
 password = WebDriverWait(driver,10).until(EC.element_to_be_clickable((By.CSS_SELECTOR,"input[name='password']"))) 
  
+# username.clear() 
+# username.send_keys("Make_it_exotic") 
+# password.clear() 
+# password.send_keys("Frias123vv") 
+
 username.clear() 
-username.send_keys("Make_it_exotic") 
+username.send_keys("castillopenal0903") 
 password.clear() 
 password.send_keys("Frias123vv") 
  
@@ -50,8 +55,8 @@ while True:
     titulo = None#   
     fuente = 'Instagram'#
     linkk = None# 
-    cometarios = []
-    likes = None
+    cometarios = []#
+    likes = None#
     try:
         
         titulo = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//span[@class='_7UhW9   xLCgt      MMzan   KV-D4           se6yk       T0kll ']")))
@@ -59,23 +64,21 @@ while True:
         titulo =titulo.text
 
         try:
-            
-            
             def recursiveclick(invertal):
                 try:
-                    contu = driver.find_element(By.XPATH,"/html/body/div[6]/div[3]/div/article/div/div[2]/div/div/div[2]/div[1]/ul/li/div/button")
+                    contu = driver.find_element(By.XPATH, "//div[@class='             qF0y9          Igw0E     IwRSH        YBx95       _4EzTm                                                                                                            NUiEW  ']")
                     contu.click()
                     time.sleep(invertal)
                     recursiveclick(4)
                 except Exception as e:
                     print(e)
+                    print("Scroll Terminado")
                     return
             
             recursiveclick(4)
 
             elemento = driver.find_elements(By.CLASS_NAME, "C4VMK")
-            for element in elemento:
-                time.sleep(2)        
+            for element in elemento:     
                 nombre =  element.find_element(By.CLASS_NAME,"_6lAjh ").text
                 comentari = element.find_element(By.CLASS_NAME,"MOdxS ")
                 coment = nombre + ": " + comentari.text
@@ -83,9 +86,10 @@ while True:
         except Exception as e:
             print(e)
 
-
-
         linkk = driver.current_url
+
+        likes = driver.find_element(By.XPATH, "/html/body/div[6]/div[3]/div/article/div/div[2]/div/div/div[2]/section[2]/div/div/div/a/div/span").text
+
 
         rightt = driver.find_elements(By.CLASS_NAME, "l8mY4")
         for left in rightt:
